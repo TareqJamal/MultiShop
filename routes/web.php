ea530697d85c3ebaf93570ce3dd834e310f01d2f
TareqJamal <tareqjamal113@gmail.com>
2023-09-29 01:56:33 +0300

    Install Mcamara and Astrotomic Package

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+    Route::get('/', function () {
+        dd( LaravelLocalization::getCurrentLocaleName()  );
+        return view('welcome');
+    });
 });
